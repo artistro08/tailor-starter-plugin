53d3771d71442d1af4faab8490ec808269134800
Devin Green <design@devingreen.me>
2023-02-08 14:01:33 -0600

    Update elements to hide due to theme restructure

---
+++ assets/js/disable_blog.js
@@ -0,0 +1,31 @@
+function queryBlogElements(selector, callback) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach((element) => callback(element));
+}
+
+function observeBlog(selector, callback) {
+  queryBlogElements(selector, callback);
+
+  const observer = new MutationObserver(() => {
+    queryBlogElements(selector, callback);
+  });
+
+  observer.observe(document.documentElement, {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree: true,
+  });
+}
+
+observeBlog(".backend-dropdownmenu .item", (element) => {
+  if (element.innerText.trim() === "Posts") {
+    element.style.display = "none";
+  }
+});
+
+observeBlog(".mainmenu-item a", (element) => {
+  if (element.innerText.trim() === "Blog") {
+    element.parentNode.style.display = "none";
+  }
+});
