5c9e7cc16d3369f78d6332cdc2b9b7cd487aa11d
Devin Green <design@devingreen.me>
2023-03-01 05:27:14 -0600

    code cleanup

--- assets/js/companion.js
+++ assets/js/companion.js
@@ -1,7 +1,5 @@
-
 document.addEventListener("page:loaded", (event) => {
     let saveButton = document.querySelectorAll('[data-request="onSave"]');
-    console.log(saveButton);
     saveButton.forEach((button) => {
         button.setAttribute("data-request-success", "window.location.reload()");
     });
